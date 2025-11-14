966e16c922b564036c55cea241c2e04e2bd6bc58
Nylan Riquelme <nylriqalb@alu.edu.gva.es>
2025-11-14 21:29:03 +0100

    tonto

--- untitled/src/main/java/yas/PracticaEntornos.java
+++ untitled/src/main/java/yas/PracticaEntornos.java
@@ -8,10 +8,13 @@ public class PracticaEntornos {
         System.out.print("Introduce tu año de nacimiento:");
         int anyo = sc.nextInt();
         LocalDateTime fechaActual = LocalDateTime.now();
-        int edad = fechaActual.getYear() + anyo;
+        int edad = fechaActual.getYear() - anyo;
+        System.out.println("---------------------------");
         if (edad > 18) {
+            System.out.println("---------------------------");
             System.out.println("Eres mayor de edad");
         } else {
+            System.out.println("---------------------------");
             System.out.println("Eres menor de edad");
         }
     }
